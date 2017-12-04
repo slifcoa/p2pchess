27fd8e62b6963e087ce3bd6bdac826ff332f3376
YoDropTable <Pr0bl3m!-GitHub>
2017-12-03 23:52:04 -0500

    Kinda Works. Server and Client Connect.

---
+++ src/chess/ServerHandler.java
@@ -0,0 +1,81 @@
+package chess;
+
+import javax.swing.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/****************************************************************
+ * Server application that can connect to several clients at a
+ * time using multithreading, request a list of the server's
+ * files, retrieve a specified file, from the server,
+ * and send and store a specified file to the server.
+ *
+ * @author Mike Ames
+ * @author Phil "The Chosen One" Garza aka Dipstick
+ * @author Zachary Hern
+ * @author Adam Slifco
+ *
+ * @version October 2017
+ ******************************************************************/
+class ServerHanlder implements Runnable{
+    /* socket the sever uses */
+    protected int           connSockNum     =   8415;
+    protected ServerSocket  myServer;
+    protected Socket        clientSocket;
+    protected boolean       isRunning       =   true;
+    protected Thread        runningThread;
+    JTextArea myOuput;
+
+    public ServerHanlder(JTextArea pointerToOutput) {
+        this.myOuput = pointerToOutput;
+    }
+
+    /******************************************************************
+     * Main method for running program based on commands.
+     ******************************************************************/
+    public void run() {
+        synchronized (this) {
+            this.runningThread = Thread.currentThread();
+        }
+        try{
+            myServer = new ServerSocket(this.connSockNum);
+        } catch (IOException e){
+            throw new RuntimeException("Cannot Open Port 8415", e);
+        }
+        outputMessage("Hosting Game on \n " + myServer.getInetAddress() + ":8415");
+        while(isRunning){
+            try {
+                clientSocket = this.myServer.accept();
+            } catch (IOException e){
+                throw new RuntimeException("Error Accepting Client", e);
+            }
+            try{
+                processClientRequest(clientSocket);
+
+            } catch (Exception e){
+                //DO STUFF
+            }
+        }
+    }
+
+    private void processClientRequest(Socket clientSocket)
+            throws Exception {
+        boolean ClientConnected = true;
+        while(ClientConnected) {
+                DataInputStream inFromClient = new DataInputStream(clientSocket.getInputStream());
+                String fromClient;
+                fromClient = inFromClient.readUTF();
+                DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream());
+                outputMessage(fromClient);
+                outToClient.writeUTF("Server 404OK");
+            }
+        }
+
+
+    private void outputMessage(String myMessage){
+        myOuput.append((myMessage + "\n"));
+    }
+}
